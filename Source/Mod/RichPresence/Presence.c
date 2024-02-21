2ce0328323a6a9d911fc6be1ebe46aa059e7ce8e
yobonez <59935203+yobonez@users.noreply.github.com>
2024-02-21 22:18:16 +0100

    Displaying tools/weapons held in the current moment

--- Source/Mod/RichPresence/Presence.c
+++ Source/Mod/RichPresence/Presence.c
@@ -4,6 +4,31 @@
 #include <Presence.h>
 
 const char* app_id = "699358451494682714";
+int player_id = 0;
+game_state old_state;
+
+const char tool_descriptions[4][MAX_TEXT] = {
+    "Digging",
+    "",
+    "Building",
+    "Throwing nades"
+};
+const char weapon_descriptions[3][MAX_TEXT] = {
+    "Rifle",
+    "SMG",
+    "Shotgun"
+};
+const char tool_images[4][MAX_TEXT] = {
+    "largeimagekey_spade",
+    "largeimagekey_block",
+    "",
+    "largeimagekey_grenade"
+};
+const char weapon_images[3][MAX_TEXT] = {
+    "largeimagekey_rifle",
+    "largeimagekey_smg",
+    "largeimagekey_shotgun"
+};
 
 // handlers that inform about something may be good for logger module perhaps?
 void handle_discord_ready(const DiscordUser* user) { return; }
@@ -29,18 +54,52 @@ void discord_shutdown() {
     Discord_Shutdown();
 }
 
+game_state get_current_game_state() {
+    game_state state;
+    memset(&state, 0, sizeof(game_state));
+
+    state.current_tool = *((int*)((client_base + 0x13cf808) + (player_id * 0x3a8)));
+    state.current_weapon = *((int*)((client_base + 0x7ce5c) + (player_id * 0x3a8)));
+    state.current_team = *((int*)((client_base + 0x7ce58) + (player_id * 0x3a8)));
+    state.intel_holder_t1 = *((int*)(client_base + 0x13cf958));
+    state.intel_holder_t2 = *((int*)(client_base + 0x13cf924));
+
+    return state;
+}
+
 void update_presence(){
     if (presence_enabled)
     {
-        int64_t playtime_start = time(0);
+        // int64_t playtime_start = time(0); // map change
+        game_state state = get_current_game_state();
 
+        if ((state.current_team == old_state.current_team) &&
+            (state.current_tool == old_state.current_tool) &&
+            (state.current_weapon == old_state.current_weapon) &&
+            (state.intel_holder_t1 == old_state.intel_holder_t1) &&
+            (state.intel_holder_t2 == old_state.intel_holder_t2)) 
+        { return;}
+
+        old_state = state;
         DiscordRichPresence presence;
         memset(&presence, 0, sizeof(DiscordRichPresence));
         presence.details = "Server placeholder";
         presence.state = "Map placeholder";
-        presence.startTimestamp = playtime_start;
-        presence.largeImageKey = "ace_of_spades";
-        presence.smallImageKey = "smallimagekey_intel";
+        // presence.startTimestamp = playtime_start;
+
+        if (state.current_tool != 2) {
+            presence.largeImageKey = tool_images[state.current_tool];
+            presence.largeImageText = tool_descriptions[state.current_tool];
+        }
+        else {
+            presence.largeImageKey = weapon_images[state.current_weapon];
+            presence.largeImageText = weapon_descriptions[state.current_weapon];
+        }
+        
+        if (state.intel_holder_t1 == player_id || state.intel_holder_t2 == player_id)
+            presence.smallImageKey = "smallimagekey_intel";
+        else
+            presence.smallImageKey = "ace_of_spades";
         Discord_UpdatePresence(&presence);
     }
     else {
@@ -49,6 +108,8 @@ void update_presence(){
 }
 
 void initrichpresence() {
+    player_id = *((int*)(client_base+0x13b1cf0));
+
     discord_init();
     update_presence();
 
--- Source/Mod/RichPresence/Presence.h
+++ Source/Mod/RichPresence/Presence.h
@@ -1,5 +1,17 @@
 #include <discord_rpc.h>
 
+#define MAX_TEXT 100
+
 extern int presence_enabled;
+extern int client_base;
 
 void initrichpresence();
+void update_presence();
+
+typedef struct game_state {
+    int current_tool;
+    int current_weapon;
+    int current_team;
+    int intel_holder_t1;
+    int intel_holder_t2;
+} game_state;
